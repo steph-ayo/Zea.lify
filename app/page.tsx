1c0da2dc127a18592224d7014cdcf52c3d5ad114
steph-ayo <popoolastephen19@gmail.com>
2025-09-02 23:28:29 +0100

    folder update

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
 import Navbar from "@/components/navbar";
 import { Spotlight } from "@/components/ui/spotlight";
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen md:itms-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="w-full h-screen md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Navbar />
-      <Spotlight
-        className="hidden md:flex md:left-80 md:-top-80"
-        fill="white"
-      />
+      <Spotlight className="hidden md:flex md:left-80" fill="white" />
+      <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-20 px-2">
+        <div className="text-4xl pb-5 md:text-7xl px-6 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to bg-neutral-400 bg-opacity-50">
+          Create, grow, and <br /> scale your business
+        </div>
+        <p className="mt-4 text-lg font-normal  text-neutral-300 max-w-lg text-center mx-auto px-4">
+          Custom tailored solutions for your business. We are a team of
+          creatives who are excited to help you grow your business.
+        </p>
+        <Link
+          href={"/book"}
+          className="cursor-pointer flex items-center justify-center border rounded-full w-48 p-2 mx-auto my-6 text-white"
+        >
+          Book a call
+        </Link>
+      </div>
     </div>
   );
 }
