3c074aa138747277e50409242cfcc13d585460d7
steph-ayo <popoolastephen19@gmail.com>
2025-10-25 11:45:17 +0100

    app update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 
 const config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
@@ -122,7 +123,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 } satisfies Config;
 
 export default config;
