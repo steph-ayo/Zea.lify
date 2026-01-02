2e0e0f771dbf773ed6a1c08392b852d603178e11
steph-ayo <popoolastephen19@gmail.com>
2026-01-02 11:36:42 +0100

    ui update

--- components/ui/3d-card.tsx
+++ components/ui/3d-card.tsx
@@ -1,18 +1,20 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-
 import React, {
   createContext,
   useState,
   useContext,
   useRef,
   useEffect,
+  useCallback,
 } from "react";
 
 const MouseEnterContext = createContext<
   [boolean, React.Dispatch<React.SetStateAction<boolean>>] | undefined
 >(undefined);
+
+/* -------------------- CARD CONTAINER -------------------- */
 
 export const CardContainer = ({
   children,
@@ -28,23 +30,27 @@ export const CardContainer = ({
 
   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
     if (!containerRef.current) return;
+
     const { left, top, width, height } =
       containerRef.current.getBoundingClientRect();
+
     const x = (e.clientX - left - width / 2) / 25;
     const y = (e.clientY - top - height / 2) / 25;
+
     containerRef.current.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
   };
 
-  const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleMouseEnter = () => {
     setIsMouseEntered(true);
-    if (!containerRef.current) return;
   };
 
-  const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleMouseLeave = () => {
     if (!containerRef.current) return;
+
     setIsMouseEntered(false);
     containerRef.current.style.transform = `rotateY(0deg) rotateX(0deg)`;
   };
+
   return (
     <MouseEnterContext.Provider value={[isMouseEntered, setIsMouseEntered]}>
       <div
@@ -52,9 +58,7 @@ export const CardContainer = ({
           "py-20 flex items-center justify-center",
           containerClassName
         )}
-        style={{
-          perspective: "1000px",
-        }}
+        style={{ perspective: "1000px" }}
       >
         <div
           ref={containerRef}
@@ -65,9 +69,7 @@ export const CardContainer = ({
             "flex items-center justify-center relative transition-all duration-200 ease-linear",
             className
           )}
-          style={{
-            transformStyle: "preserve-3d",
-          }}
+          style={{ transformStyle: "preserve-3d" }}
         >
           {children}
         </div>
@@ -75,6 +77,8 @@ export const CardContainer = ({
     </MouseEnterContext.Provider>
   );
 };
+
+/* -------------------- CARD BODY -------------------- */
 
 export const CardBody = ({
   children,
@@ -86,7 +90,7 @@ export const CardBody = ({
   return (
     <div
       className={cn(
-        "h-96 w-96 [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]",
+        "h-96 w-96 [transform-style:preserve-3d] [&>*]:[transform-style:preserve-3d]",
         className
       )}
     >
@@ -94,6 +98,8 @@ export const CardBody = ({
     </div>
   );
 };
+
+/* -------------------- CARD ITEM -------------------- */
 
 export const CardItem = ({
   as: Tag = "div",
@@ -121,18 +127,41 @@ export const CardItem = ({
   const ref = useRef<HTMLDivElement>(null);
   const [isMouseEntered] = useMouseEnter();
 
+  const handleAnimations = useCallback(() => {
+    if (!ref.current) return;
+
+    if (isMouseEntered) {
+      ref.current.style.transform = `
+        translateX(${translateX}px)
+        translateY(${translateY}px)
+        translateZ(${translateZ}px)
+        rotateX(${rotateX}deg)
+        rotateY(${rotateY}deg)
+        rotateZ(${rotateZ}deg)
+      `;
+    } else {
+      ref.current.style.transform = `
+        translateX(0px)
+        translateY(0px)
+        translateZ(0px)
+        rotateX(0deg)
+        rotateY(0deg)
+        rotateZ(0deg)
+      `;
+    }
+  }, [
+    isMouseEntered,
+    translateX,
+    translateY,
+    translateZ,
+    rotateX,
+    rotateY,
+    rotateZ,
+  ]);
+
   useEffect(() => {
     handleAnimations();
-  }, [isMouseEntered]);
-
-  const handleAnimations = () => {
-    if (!ref.current) return;
-    if (isMouseEntered) {
-      ref.current.style.transform = `translateX(${translateX}px) translateY(${translateY}px) translateZ(${translateZ}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg)`;
-    } else {
-      ref.current.style.transform = `translateX(0px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
-    }
-  };
+  }, [handleAnimations]);
 
   return (
     <Tag
@@ -145,11 +174,14 @@ export const CardItem = ({
   );
 };
 
-// Create a hook to use the context
+/* -------------------- CONTEXT HOOK -------------------- */
+
 export const useMouseEnter = () => {
   const context = useContext(MouseEnterContext);
+
   if (context === undefined) {
     throw new Error("useMouseEnter must be used within a MouseEnterProvider");
   }
+
   return context;
 };
--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -1,3 +1,193 @@
+// // Inspired by react-hot-toast library
+// import * as React from "react";
+
+// import type { ToastActionElement, ToastProps } from "@/components/ui/toast";
+
+// const TOAST_LIMIT = 1;
+// const TOAST_REMOVE_DELAY = 1000000;
+
+// type ToasterToast = ToastProps & {
+//   id: string;
+//   title?: React.ReactNode;
+//   description?: React.ReactNode;
+//   action?: ToastActionElement;
+// };
+
+// const actionTypes = {
+//   ADD_TOAST: "ADD_TOAST",
+//   UPDATE_TOAST: "UPDATE_TOAST",
+//   DISMISS_TOAST: "DISMISS_TOAST",
+//   REMOVE_TOAST: "REMOVE_TOAST",
+// } as const;
+
+// let count = 0;
+
+// function genId() {
+//   count = (count + 1) % Number.MAX_SAFE_INTEGER;
+//   return count.toString();
+// }
+
+// type ActionType = typeof actionTypes;
+
+// type Action =
+//   | {
+//       type: ActionType["ADD_TOAST"];
+//       toast: ToasterToast;
+//     }
+//   | {
+//       type: ActionType["UPDATE_TOAST"];
+//       toast: Partial<ToasterToast>;
+//     }
+//   | {
+//       type: ActionType["DISMISS_TOAST"];
+//       toastId?: ToasterToast["id"];
+//     }
+//   | {
+//       type: ActionType["REMOVE_TOAST"];
+//       toastId?: ToasterToast["id"];
+//     };
+
+// interface State {
+//   toasts: ToasterToast[];
+// }
+
+// const toastTimeouts = new Map<string, ReturnType<typeof setTimeout>>();
+
+// const addToRemoveQueue = (toastId: string) => {
+//   if (toastTimeouts.has(toastId)) {
+//     return;
+//   }
+
+//   const timeout = setTimeout(() => {
+//     toastTimeouts.delete(toastId);
+//     dispatch({
+//       type: "REMOVE_TOAST",
+//       toastId: toastId,
+//     });
+//   }, TOAST_REMOVE_DELAY);
+
+//   toastTimeouts.set(toastId, timeout);
+// };
+
+// export const reducer = (state: State, action: Action): State => {
+//   switch (action.type) {
+//     case "ADD_TOAST":
+//       return {
+//         ...state,
+//         toasts: [action.toast, ...state.toasts].slice(0, TOAST_LIMIT),
+//       };
+
+//     case "UPDATE_TOAST":
+//       return {
+//         ...state,
+//         toasts: state.toasts.map((t) =>
+//           t.id === action.toast.id ? { ...t, ...action.toast } : t
+//         ),
+//       };
+
+//     case "DISMISS_TOAST": {
+//       const { toastId } = action;
+
+//       // ! Side effects ! - This could be extracted into a dismissToast() action,
+//       // but I'll keep it here for simplicity
+//       if (toastId) {
+//         addToRemoveQueue(toastId);
+//       } else {
+//         state.toasts.forEach((toast) => {
+//           addToRemoveQueue(toast.id);
+//         });
+//       }
+
+//       return {
+//         ...state,
+//         toasts: state.toasts.map((t) =>
+//           t.id === toastId || toastId === undefined
+//             ? {
+//                 ...t,
+//                 open: false,
+//               }
+//             : t
+//         ),
+//       };
+//     }
+//     case "REMOVE_TOAST":
+//       if (action.toastId === undefined) {
+//         return {
+//           ...state,
+//           toasts: [],
+//         };
+//       }
+//       return {
+//         ...state,
+//         toasts: state.toasts.filter((t) => t.id !== action.toastId),
+//       };
+//   }
+// };
+
+// const listeners: Array<(state: State) => void> = [];
+
+// let memoryState: State = { toasts: [] };
+
+// function dispatch(action: Action) {
+//   memoryState = reducer(memoryState, action);
+//   listeners.forEach((listener) => {
+//     listener(memoryState);
+//   });
+// }
+
+// type Toast = Omit<ToasterToast, "id">;
+
+// function toast({ ...props }: Toast) {
+//   const id = genId();
+
+//   const update = (props: ToasterToast) =>
+//     dispatch({
+//       type: "UPDATE_TOAST",
+//       toast: { ...props, id },
+//     });
+//   const dismiss = () => dispatch({ type: "DISMISS_TOAST", toastId: id });
+
+//   dispatch({
+//     type: "ADD_TOAST",
+//     toast: {
+//       ...props,
+//       id,
+//       open: true,
+//       onOpenChange: (open) => {
+//         if (!open) dismiss();
+//       },
+//     },
+//   });
+
+//   return {
+//     id: id,
+//     dismiss,
+//     update,
+//   };
+// }
+
+// function useToast() {
+//   const [state, setState] = React.useState<State>(memoryState);
+
+//   React.useEffect(() => {
+//     listeners.push(setState);
+//     return () => {
+//       const index = listeners.indexOf(setState);
+//       if (index > -1) {
+//         listeners.splice(index, 1);
+//       }
+//     };
+//   }, [state]);
+
+//   return {
+//     ...state,
+//     toast,
+//     dismiss: (toastId?: string) => dispatch({ type: "DISMISS_TOAST", toastId }),
+//   };
+// }
+
+// export { useToast, toast };
+
 // Inspired by react-hot-toast library
 import * as React from "react";
 
@@ -13,12 +203,39 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement;
 };
 
-const actionTypes = {
-  ADD_TOAST: "ADD_TOAST",
-  UPDATE_TOAST: "UPDATE_TOAST",
-  DISMISS_TOAST: "DISMISS_TOAST",
-  REMOVE_TOAST: "REMOVE_TOAST",
-} as const;
+/* -------------------- ACTION TYPES -------------------- */
+
+type ActionType =
+  | "ADD_TOAST"
+  | "UPDATE_TOAST"
+  | "DISMISS_TOAST"
+  | "REMOVE_TOAST";
+
+type Action =
+  | {
+      type: "ADD_TOAST";
+      toast: ToasterToast;
+    }
+  | {
+      type: "UPDATE_TOAST";
+      toast: Partial<ToasterToast>;
+    }
+  | {
+      type: "DISMISS_TOAST";
+      toastId?: ToasterToast["id"];
+    }
+  | {
+      type: "REMOVE_TOAST";
+      toastId?: ToasterToast["id"];
+    };
+
+/* -------------------- STATE -------------------- */
+
+interface State {
+  toasts: ToasterToast[];
+}
+
+/* -------------------- ID GENERATION -------------------- */
 
 let count = 0;
 
@@ -27,47 +244,25 @@ function genId() {
   return count.toString();
 }
 
-type ActionType = typeof actionTypes;
-
-type Action =
-  | {
-      type: ActionType["ADD_TOAST"];
-      toast: ToasterToast;
-    }
-  | {
-      type: ActionType["UPDATE_TOAST"];
-      toast: Partial<ToasterToast>;
-    }
-  | {
-      type: ActionType["DISMISS_TOAST"];
-      toastId?: ToasterToast["id"];
-    }
-  | {
-      type: ActionType["REMOVE_TOAST"];
-      toastId?: ToasterToast["id"];
-    };
-
-interface State {
-  toasts: ToasterToast[];
-}
+/* -------------------- TIMEOUT MANAGEMENT -------------------- */
 
 const toastTimeouts = new Map<string, ReturnType<typeof setTimeout>>();
 
 const addToRemoveQueue = (toastId: string) => {
-  if (toastTimeouts.has(toastId)) {
-    return;
-  }
+  if (toastTimeouts.has(toastId)) return;
 
   const timeout = setTimeout(() => {
     toastTimeouts.delete(toastId);
     dispatch({
       type: "REMOVE_TOAST",
-      toastId: toastId,
+      toastId,
     });
   }, TOAST_REMOVE_DELAY);
 
   toastTimeouts.set(toastId, timeout);
 };
+
+/* -------------------- REDUCER -------------------- */
 
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
@@ -88,8 +283,6 @@ export const reducer = (state: State, action: Action): State => {
     case "DISMISS_TOAST": {
       const { toastId } = action;
 
-      // ! Side effects ! - This could be extracted into a dismissToast() action,
-      // but I'll keep it here for simplicity
       if (toastId) {
         addToRemoveQueue(toastId);
       } else {
@@ -101,22 +294,16 @@ export const reducer = (state: State, action: Action): State => {
       return {
         ...state,
         toasts: state.toasts.map((t) =>
-          t.id === toastId || toastId === undefined
-            ? {
-                ...t,
-                open: false,
-              }
-            : t
+          t.id === toastId || toastId === undefined ? { ...t, open: false } : t
         ),
       };
     }
+
     case "REMOVE_TOAST":
       if (action.toastId === undefined) {
-        return {
-          ...state,
-          toasts: [],
-        };
+        return { ...state, toasts: [] };
       }
+
       return {
         ...state,
         toasts: state.toasts.filter((t) => t.id !== action.toastId),
@@ -124,20 +311,22 @@ export const reducer = (state: State, action: Action): State => {
   }
 };
 
+/* -------------------- STORE -------------------- */
+
 const listeners: Array<(state: State) => void> = [];
 
 let memoryState: State = { toasts: [] };
 
 function dispatch(action: Action) {
   memoryState = reducer(memoryState, action);
-  listeners.forEach((listener) => {
-    listener(memoryState);
-  });
+  listeners.forEach((listener) => listener(memoryState));
 }
+
+/* -------------------- TOAST API -------------------- */
 
 type Toast = Omit<ToasterToast, "id">;
 
-function toast({ ...props }: Toast) {
+function toast(props: Toast) {
   const id = genId();
 
   const update = (props: ToasterToast) =>
@@ -145,6 +334,7 @@ function toast({ ...props }: Toast) {
       type: "UPDATE_TOAST",
       toast: { ...props, id },
     });
+
   const dismiss = () => dispatch({ type: "DISMISS_TOAST", toastId: id });
 
   dispatch({
@@ -160,11 +350,13 @@ function toast({ ...props }: Toast) {
   });
 
   return {
-    id: id,
+    id,
     dismiss,
     update,
   };
 }
+
+/* -------------------- HOOK -------------------- */
 
 function useToast() {
   const [state, setState] = React.useState<State>(memoryState);
@@ -173,11 +365,9 @@ function useToast() {
     listeners.push(setState);
     return () => {
       const index = listeners.indexOf(setState);
-      if (index > -1) {
-        listeners.splice(index, 1);
-      }
+      if (index > -1) listeners.splice(index, 1);
     };
-  }, [state]);
+  }, []);
 
   return {
     ...state,
